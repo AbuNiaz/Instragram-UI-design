43a5a3935018bac228614245e7a28e4049b061d1
Abu Niaz <niaz35-507@diu.edu.bd>
2022-03-15 10:58:02 +0600

    yes

---
+++ lib/home_one.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: currentPage == 0
+                      ? const Color.fromARGB(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.search,
+                  color: currentPage == 1
+                      ? const Color.fromARGB(203, 73, 101, 1)
+                      : const Color.fromRGBO(40, 40, 40, 1),
+                )),
+            const Spacer(),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.ondemand_video)),
+            const Spacer(),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.card_travel)),
+            const Spacer(),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instragram_ui/home_one.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,60 +18,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentPage = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: [
-            const Spacer(),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentPage = 0;
-                  });
-                },
-                icon: Icon(
-                  Icons.home,
-                  color: currentPage == 0
-                      ? const Color.fromARGB(203, 73, 101, 1)
-                      : const Color.fromRGBO(40, 40, 40, 1),
-                )),
-            const Spacer(),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    currentPage = 1;
-                  });
-                },
-                icon: Icon(
-                  Icons.search,
-                  color: currentPage == 1
-                      ? const Color.fromARGB(203, 73, 101, 1)
-                      : const Color.fromRGBO(40, 40, 40, 1),
-                )),
-            const Spacer(),
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.ondemand_video)),
-            const Spacer(),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.card_travel)),
-            const Spacer(),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
-            const Spacer(),
-          ],
-        ),
-      ),
     );
   }
 }
