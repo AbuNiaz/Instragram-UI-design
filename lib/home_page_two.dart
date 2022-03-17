a5aedb7960a124e37a6ec33eb27cde68274dfab3
Abu Niaz <niaz35-507@diu.edu.bd>
2022-03-17 12:01:47 +0600

    Update home_page_two.dart

--- lib/home_page_two.dart
+++ lib/home_page_two.dart
@@ -11,21 +11,36 @@ class _HomePageTwoState extends State<HomePageTwo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: SizedBox(
-          child: Image.asset(
-            "images/instagram.jpg",
-            height: 100,
-            width: 150,
+        appBar: AppBar(
+          title: SizedBox(
+            child: Image.asset(
+              "images/instagram.jpg",
+              height: 100,
+              width: 150,
+            ),
           ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.add_circle)),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.chat_bubble_outline)),
+          ],
         ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add_circle)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.chat_bubble_outline)),
-        ],
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                    children: List.generate(
+                        8,
+                        (index) => Container(
+                            padding: const EdgeInsets.all(10),
+                            child: const CircleAvatar()))),
+              )
+            ],
+          ),
+        ));
   }
 }
